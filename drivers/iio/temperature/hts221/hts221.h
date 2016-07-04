432c615d48069fb32313433a1634fcfc13975ef7
Lorenzo Bianconi <lorenzo.bianconi@st.com>
2016-07-04 21:01:55 +0200

    hts221: add spi support (not tested yet)

--- drivers/iio/temperature/hts221/hts221.h
+++ drivers/iio/temperature/hts221/hts221.h
@@ -15,6 +15,17 @@
 
 #include <linux/iio/iio.h>
 
+#if defined(CONFIG_IIO_HTS221_SPI) || \
+    defined(CONFIG_IIO_HTS221_SPI_MODULE)
+#define HTS221_RX_MAX_LENGTH	500
+#define HTS221_TX_MAX_LENGTH	500
+
+struct hts221_transfer_buffer {
+	u8 rx_buf[HTS221_RX_MAX_LENGTH];
+	u8 tx_buf[HTS221_TX_MAX_LENGTH] ____cacheline_aligned;
+};
+#endif /* CONFIG_IIO_HTS221_SPI */
+
 struct hts221_transfer_function {
 	int (*read)(struct device *dev, u8 addr, int len, u8 *data);
 	int (*write)(struct device *dev, u8 addr, int len, u8 *data);
@@ -57,6 +68,10 @@ struct hts221_dev {
 	struct hts221_sensor sensors[HTS221_SENSOR_MAX];
 
 	const struct hts221_transfer_function *tf;
+#if defined(CONFIG_IIO_HTS221_SPI) || \
+    defined(CONFIG_IIO_HTS221_SPI_MODULE)
+	struct hts221_transfer_buffer tb;
+#endif /* CONFIG_IIO_HTS221_SPI */
 };
 
 /**
