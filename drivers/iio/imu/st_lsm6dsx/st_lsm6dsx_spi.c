c254f934ffe65dcfdcbcd7ac5f554c0b168aa856
lorenzo <me@lorenzobianconi.net>
2016-12-07 15:39:22 +0100

    iio: imu: st_lsm6dsx: use spi_write() in st_lsm6dsx_spi_write()

--- drivers/iio/imu/st_lsm6dsx/st_lsm6dsx_spi.c
+++ drivers/iio/imu/st_lsm6dsx/st_lsm6dsx_spi.c
@@ -53,22 +53,19 @@ static int st_lsm6dsx_spi_read(struct device *dev, u8 addr, int len,
 static int st_lsm6dsx_spi_write(struct device *dev, u8 addr, int len,
 				u8 *data)
 {
-	struct spi_device *spi = to_spi_device(dev);
-	struct st_lsm6dsx_hw *hw = spi_get_drvdata(spi);
-
-	struct spi_transfer xfers = {
-		.tx_buf = hw->tb.tx_buf,
-		.bits_per_word = 8,
-		.len = len + 1,
-	};
+	struct st_lsm6dsx_hw *hw;
+	struct spi_device *spi;
 
 	if (len >= ST_LSM6DSX_TX_MAX_LENGTH)
 		return -ENOMEM;
 
+	spi = to_spi_device(dev);
+	hw = spi_get_drvdata(spi);
+
 	hw->tb.tx_buf[0] = addr;
 	memcpy(&hw->tb.tx_buf[1], data, len);
 
-	return spi_sync_transfer(spi, &xfers, 1);
+	return spi_write(spi, hw->tb.tx_buf, len + 1);
 }
 
 static const struct st_lsm6dsx_transfer_function st_lsm6dsx_transfer_fn = {
