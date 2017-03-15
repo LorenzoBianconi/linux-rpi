cf817a699fc2a6e4184d67cee26e9b06acbca89c
Lorenzo Bianconi <lorenzo.bianconi@st.com>
2017-03-15 11:21:23 +0100

    iio: misc: rename st_stail in st_stile

--- drivers/iio/misc/st_stile_usb.c
+++ drivers/iio/misc/st_stile_usb.c
@@ -1,5 +1,5 @@
 /*
- * STMicroelectronics st_stail usb driver
+ * STMicroelectronics st_stile usb driver
  *
  * Copyright 2017 STMicroelectronics Inc.
  *
@@ -11,11 +11,11 @@
 #include <linux/kernel.h>
 #include <linux/usb.h>
 
-#include "st_stail.h"
+#include "st_stile.h"
 
-static int st_stail_usb_read(struct device *dev, u8 addr, int len, u8 *data)
+static int st_stile_usb_read(struct device *dev, u8 addr, int len, u8 *data)
 {
-	struct st_stail_usb *udata = dev_get_drvdata(dev);
+	struct st_stile_usb *udata = dev_get_drvdata(dev);
 	struct usb_device *udev = udata->udev;
 	int count, err;
 
@@ -25,9 +25,9 @@ static int st_stail_usb_read(struct device *dev, u8 addr, int len, u8 *data)
 	return err < 0 ? err : count;
 }
 
-static int st_stail_usb_write(struct device *dev, u8 addr, int len, u8 *data)
+static int st_stile_usb_write(struct device *dev, u8 addr, int len, u8 *data)
 {
-	struct st_stail_usb *udata = dev_get_drvdata(dev);
+	struct st_stile_usb *udata = dev_get_drvdata(dev);
 	struct usb_device *udev = udata->udev;
 	int count;
 
@@ -35,9 +35,9 @@ static int st_stail_usb_write(struct device *dev, u8 addr, int len, u8 *data)
 			    data, len, &count, 10 * HZ);
 }
 
-static int st_stail_usb_set_enable(struct device *dev, bool state)
+static int st_stile_usb_set_enable(struct device *dev, bool state)
 {
-	struct st_stail_usb *udata = dev_get_drvdata(dev);
+	struct st_stile_usb *udata = dev_get_drvdata(dev);
 	int err = 0;
 
 	if (state)
@@ -47,40 +47,40 @@ static int st_stail_usb_set_enable(struct device *dev, bool state)
 	return err;
 }
 
-static const struct st_stail_transfer_function st_stail_usb_tf = {
-	.enable = st_stail_usb_set_enable,
-	.write = st_stail_usb_write,
-	.read = st_stail_usb_read,
+static const struct st_stile_transfer_function st_stile_usb_tf = {
+	.enable = st_stile_usb_set_enable,
+	.write = st_stile_usb_write,
+	.read = st_stile_usb_read,
 };
 
-static const struct usb_device_id st_stail_usb_id_table[] = {
+static const struct usb_device_id st_stile_usb_id_table[] = {
 	{ USB_DEVICE(0x0483, 0x5740) },
 	{}
 };
-MODULE_DEVICE_TABLE(usb, st_stail_usb_id_table);
+MODULE_DEVICE_TABLE(usb, st_stile_usb_id_table);
 
-static void st_stail_usb_complete(struct urb *urb)
+static void st_stile_usb_complete(struct urb *urb)
 {
-	struct st_stail_usb *udata = urb->context;
+	struct st_stile_usb *udata = urb->context;
 
 	if (!urb->status) {
-		struct st_stail_hw *hw;
+		struct st_stile_hw *hw;
 
-		hw = container_of(udata, struct st_stail_hw, usb);
-		st_stail_trigger_handler(hw, udata->buff);
+		hw = container_of(udata, struct st_stile_hw, usb);
+		st_stile_trigger_handler(hw, udata->buff);
 	}
 	
 	if (udata->resched)
 		usb_submit_urb(urb, GFP_KERNEL);
 }
 
-static int st_stail_usb_probe(struct usb_interface *interface,
+static int st_stile_usb_probe(struct usb_interface *interface,
 			      const struct usb_device_id *id)
 {
 	struct usb_host_interface *cur_setting = interface->cur_altsetting;
 	struct usb_device *udev = interface_to_usbdev(interface);
 	struct usb_endpoint_descriptor *ep;
-	struct st_stail_hw *hw;
+	struct st_stile_hw *hw;
 	int i;
 
 	hw = devm_kzalloc(&interface->dev, sizeof(*hw), GFP_KERNEL);
@@ -103,11 +103,11 @@ static int st_stail_usb_probe(struct usb_interface *interface,
 		return -ENODEV;
 
 	usb_set_intfdata(interface, &hw->usb);
-	hw->tf = &st_stail_usb_tf;
+	hw->tf = &st_stile_usb_tf;
 	hw->dev = &interface->dev;
 	hw->usb.udev = udev;
 
-	hw->usb.buff = usb_alloc_coherent(udev, ST_STAIL_BUFF_SIZE,
+	hw->usb.buff = usb_alloc_coherent(udev, ST_STILE_BUFF_SIZE,
 					  GFP_KERNEL, &hw->usb.dma_buff);
 	if (!hw->usb.buff)
 		return -ENOMEM;
@@ -119,31 +119,31 @@ static int st_stail_usb_probe(struct usb_interface *interface,
 
 	usb_fill_bulk_urb(hw->usb.urb, udev,
 			  usb_rcvbulkpipe(udev, hw->usb.in_addr),
-			  hw->usb.buff, ST_STAIL_BUFF_SIZE,
-			  st_stail_usb_complete, &hw->usb);
+			  hw->usb.buff, ST_STILE_BUFF_SIZE,
+			  st_stile_usb_complete, &hw->usb);
 	/* enable DMA transfer */
 	hw->usb.urb->transfer_flags |= URB_NO_TRANSFER_DMA_MAP;
 	hw->usb.urb->transfer_dma = hw->usb.dma_buff;
 
-	return st_stail_probe(hw);
+	return st_stile_probe(hw);
 }
 
-static void st_stail_usb_disconnect(struct usb_interface *interface)
+static void st_stile_usb_disconnect(struct usb_interface *interface)
 {
-	struct st_stail_usb *udata = usb_get_intfdata(interface);
+	struct st_stile_usb *udata = usb_get_intfdata(interface);
 
 	usb_kill_urb(udata->urb);
 	usb_free_urb(udata->urb);
 }
 
-static struct usb_driver st_stail_usb_driver = {
-	.name = "st_stail",
-	.id_table = st_stail_usb_id_table,
-	.probe = st_stail_usb_probe,
-	.disconnect = st_stail_usb_disconnect,
+static struct usb_driver st_stile_usb_driver = {
+	.name = "st_stile",
+	.id_table = st_stile_usb_id_table,
+	.probe = st_stile_usb_probe,
+	.disconnect = st_stile_usb_disconnect,
 };
-module_usb_driver(st_stail_usb_driver);
+module_usb_driver(st_stile_usb_driver);
 
 MODULE_AUTHOR("Lorenzo Bianconi <lorenzo.bianconi@st.com>");
-MODULE_DESCRIPTION("STMicroelectronics st_stail usb driver");
+MODULE_DESCRIPTION("STMicroelectronics st_stile usb driver");
 MODULE_LICENSE("GPL v2");
