2b5989455de1446fa458fa52c8cc709cee31a8eb
Lorenzo Bianconi <lorenzo.bianconi@st.com>
2017-06-16 09:27:39 +0200

    iio: accel: st_accel_spi: add 3 wire support (not completed yet)

--- drivers/iio/accel/st_accel_spi.c
+++ drivers/iio/accel/st_accel_spi.c
@@ -18,6 +18,16 @@
 #include <linux/iio/common/st_sensors_spi.h>
 #include "st_accel.h"
 
+static const struct st_sensor_sim st_accel_sim_table[] = {
+	{
+		.ids = {
+			[0] = LSM303AGR_ACCEL_DEV_NAME,
+		},
+		.addr = 0x23,
+		.val = BIT(0),
+	},
+};
+
 static int st_accel_spi_probe(struct spi_device *spi)
 {
 	struct iio_dev *indio_dev;
@@ -30,7 +40,11 @@ static int st_accel_spi_probe(struct spi_device *spi)
 
 	adata = iio_priv(indio_dev);
 
-	st_sensors_spi_configure(indio_dev, spi, adata);
+	err = st_sensors_spi_configure(indio_dev, spi, adata,
+				       st_accel_sim_table,
+				       ARRAY_SIZE(st_accel_sim_table));
+	if (err < 0)
+		return err;
 
 	err = st_accel_common_probe(indio_dev);
 	if (err < 0)
@@ -61,17 +75,27 @@ static const struct spi_device_id st_accel_id_table[] = {
 MODULE_DEVICE_TABLE(spi, st_accel_id_table);
 
 #ifdef CONFIG_OF
-static const struct of_device_id lis302dl_spi_dt_ids[] = {
-	{ .compatible = "st,lis302dl-spi" },
+static const struct of_device_id st_accel_of_match[] = {
+	{
+		.compatible = "st,lis302dl-spi"
+	},
+	{
+		.compatible = "st,lis3dh-accel",
+		.data = LIS3DH_ACCEL_DEV_NAME,
+	},
+	{
+		.compatible = "st,lsm303agr-accel",
+		.data = LSM303AGR_ACCEL_DEV_NAME,
+	},
 	{}
 };
-MODULE_DEVICE_TABLE(of, lis302dl_spi_dt_ids);
+MODULE_DEVICE_TABLE(of, st_accel_of_match);
 #endif
 
 static struct spi_driver st_accel_driver = {
 	.driver = {
 		.name = "st-accel-spi",
-		.of_match_table = of_match_ptr(lis302dl_spi_dt_ids),
+		.of_match_table = of_match_ptr(st_accel_of_match),
 	},
 	.probe = st_accel_spi_probe,
 	.remove = st_accel_spi_remove,
